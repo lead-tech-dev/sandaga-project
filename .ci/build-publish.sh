8017a6d0cb8df041d536b09c84324995bb8940fe
Eric Maximan <ericmaximan@gmail.com>
2024-02-21 13:00:58 +0100

    update payment component

---
+++ .ci/build-publish.sh
@@ -0,0 +1,12 @@
+: "${USERNAME:?USERNAME not set or empty}}"
+: "${REPO:?REPO not set or empty}"
+: "${TAG:?TAG not set or empty}"
+
+docker buildx create --use
+docker buildx buiild \
+  --platform=linux/amd64, linux/arm64 \
+  -t "${USERNAME}/${REPO}:${TAG}" \
+  -t "${USERNAME}/${REPO}:latest" \
+  "${@:2}" \
+  --push
+  "$1"
